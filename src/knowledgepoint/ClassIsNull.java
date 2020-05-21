dcb4a32b9bee281c4cc09cda7b5c7268af6826b5
ThunderPicka <xsy534393044@163.com>
2020-05-21 16:49:57 +0800

    修改判断对象是否为空

--- src/knowledgepoint/ClassIsNull.java
+++ src/knowledgepoint/ClassIsNull.java
@@ -2,7 +2,9 @@ package knowledgepoint;
 
 public class ClassIsNull {
     public static boolean isNull(Object o){
-        
+        if(true){
+            System.out.println(111);
+        }
         return true;
     }
 }
